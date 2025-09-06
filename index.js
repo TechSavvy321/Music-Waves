0614098adc911164e7787a59596940725000db5c
Thasneem <117170043+Thasneem222@users.noreply.github.com>
2025-09-06 21:54:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".slavic").addEventListener("click",function(){
+    new Audio("./songs/Filip Lackovic - Slavic Queen.mp3").play();
+});
+
+document.querySelector(".odnogo").addEventListener("click",function(){
+    new Audio("./songs/ODNOGO ULTRAFUNK [SLOWED] TIKTOK VERSION.mp3").play();
+});
