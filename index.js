f9829b01310019550a72696a0a807406b0cc496c
Thasneem <117170043+Thasneem222@users.noreply.github.com>
2025-09-08 00:28:56 +0530

    index.js

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ document.querySelector(".slavic").addEventListener("click",function(){
 document.querySelector(".odnogo").addEventListener("click",function(){
     new Audio("./songs/ODNOGO ULTRAFUNK [SLOWED] TIKTOK VERSION.mp3").play();
 });
+
+document.querySelector(".closer").addEventListener("click",function(){
+    new Audio ("./songs/The Chainsmokers - Closer (Lyric) ft. Halsey.mp3").play();
+});
+
+document.querySelector(".byebyebye").addEventListener("click",function(){
+    new Audio ("./songs/Bye Bye Bye (Music Video) Deadpool & Wolverine Opening Scene Soundtrack MV.mp3").play();
+});
+
+document.querySelector(".daylight").addEventListener("click",function(){
+    new Audio ("./songs/Daylight (Official Music Video).mp3").play();
+});
+
+document.querySelector(".loveyou").addEventListener("click",function(){
+    new Audio ("./songs/Let Me Love You.mp3").play();
+});
